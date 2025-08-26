cf65992a220dd0aa740ba7f02baffde533d51daf
maximo-ale <maximoale20000@gmail.com>
2025-08-26 16:39:35 -0300

    Fixed Jest error while using 'express-rate-limit' middleware

--- middlewares/rate-limit.js
+++ middlewares/rate-limit.js
@@ -1,13 +1,19 @@
 import limiter from 'express-rate-limit';
 
-export const rateLimit = limiter({
-    windowMs: 15 * 60 * 1000,
-    max: 15,
+const isTest = process.env.NODE_ENV === 'test';
+
+export const rateLimit = isTest
+    ? (req, res, next) => next()
+    : limiter({
+    windowMs: 5 * 60 * 1000,
+    max: 20,
     message: 'Too many requests, try again later',
 });
 
-export const registerAndLoginLimit = limiter({
-    windowMs: 5 * 60 * 1000,
+export const registerAndLoginLimit = isTest
+    ? (req, res, next) => next()
+    : limiter({
+    windowMs: 2 * 60 * 1000,
     max: 10,
     message: 'Too many register and login requests, try again later',
 });
